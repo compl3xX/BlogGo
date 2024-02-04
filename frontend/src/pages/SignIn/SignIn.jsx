969caec71fb80e38190c151a0f8df5a1d79e5e2e
Rishu Tiwari <86428716+compl3xX@users.noreply.github.com>
2024-02-04 22:43:18 +0530

    User Login

--- frontend/src/pages/SignIn/SignIn.jsx
+++ frontend/src/pages/SignIn/SignIn.jsx
@@ -1,20 +1,54 @@
-import React from 'react'
-
-import './SignIn.scss'
+import { useState } from 'react'
 import { Button } from "../../components"
+import { useNavigate } from "react-router-dom";
+import axios from 'axios'
+import './SignIn.scss'
 
-const signInHandler = () => {
-
-}
 
 const SignIn = () => {
 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("")
+    const navigate = useNavigate()
+
+    const signInHandler = async (e) => {
+
+        e.preventDefault();
+
+        //send username,password and get token
+        try {
+
+            const { data } = await axios.post('http://localhost:4000/api/user/signIn', { username, password })
+            localStorage.setItem('token', data.token)
+            navigate('/home')
+
+            console.log(data)
+
+        }
+        catch (err) {
+            setError(err.response.data.error)
+            console.log(err.response.data.error)
+        }
+
+    }
+
     return (
-        <div className="sign_in">
-            <h2>Login</h2>
-            <input placeholder=" Enter username..." />
-            <input placeholder=" Enter password..." />
-            <Button handel={signInHandler} BntText="Login" />
+        <div className="sign_in_container">
+
+            <div className="sign_in_fields">
+                {error && <div className="error">{error}</div>}
+                <h1>Login</h1>
+                <input placeholder=" Enter username..."
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                />
+                <input placeholder=" Enter password..."
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password }
+                />
+                <Button handel={signInHandler} BntText="Login" />
+            </div>
         </div>
     )
 }
