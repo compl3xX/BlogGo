031d040c3ac24adcf6ce1d4cdbd68f1f21f4e6c0
Rishu Tiwari <86428716+compl3xX@users.noreply.github.com>
2024-02-19 23:19:50 +0530

    navbar : userprofile

--- frontend/src/components/Navbar/NavBar.jsx
+++ frontend/src/components/Navbar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink, useLocation } from "react-router-dom"
 import './NavBar.scss'
+import avatar from "../../assests/avatar.png"
 
 const NavBar = () => {
 
@@ -15,7 +16,10 @@ const NavBar = () => {
             <NavLink to='/home'>
                 <h3 >BlogGo</h3>
             </NavLink>
-            {hidden && <NavLink className="create" to='/createpost'>Create</NavLink>}
+            {hidden && <div className="navbar_left">
+                <NavLink className="create" to='/createpost'>Create</NavLink>
+                <NavLink className="profile"><img src={avatar} /></NavLink>
+            </div>}
             {/* {hidden && <>
                 <NavLink to='/signup'>Register</NavLink>
                 <NavLink to='/signin'>Login</NavLink>
