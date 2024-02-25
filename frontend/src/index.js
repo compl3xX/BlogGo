9d4ff417c35515f72a754cb57f48b58d3ff06753
Rishu Tiwari <86428716+compl3xX@users.noreply.github.com>
2024-02-25 23:52:34 +0530

    Deploy issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 
-import AuthContextProvider from "./context/AuthContext"
+import AuthContextProvider from "./context/AuthContext.js"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
